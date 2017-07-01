36676bb55a4e6e1b23fbd8de251dfd105fd0717a
Alintya <mystery-j@web.de>
2017-07-02 00:45:16 +0200

    le feeder

---
+++ Run it down mid/Run it down mid/Feeder.cs
@@ -0,0 +1,47 @@
+﻿using HesaEngine.SDK;
+using HesaEngine.SDK.Enums;
+using SharpDX;
+
+namespace Run_it_down_mid
+{
+    public class Feeder : IScript
+    {
+        public void OnInitialize()
+        {
+            Game.OnGameLoaded += Game_OnGameLoaded;
+        }
+
+        public string Name => "Feeder";
+        public string Version => Utils.GetVersion();
+        public string Author => "Mystery";
+
+        private static Menu RootMenu { get; set; }
+
+        private static readonly Vector3 OrderSpawn = new Vector3(395, 460, 170);
+        private static readonly Vector3 ChaosSpawn = new Vector3(14340, 14390, 180);
+
+        private void Game_OnGameLoaded()
+        {
+            Logger.Log("Loading " + Name);
+
+            // Init
+            RootMenu = new Menu("Feeder");
+            RootMenu.Add(new MenuCheckbox("enabled", "Enabled", false));
+
+            // Event subscriptions
+            Game.OnTick += Game_OnTick;
+
+            Utils.PrintChat($"v{Version} by {Author} loaded!");
+        }
+
+        private void Game_OnTick()
+        {
+            if (!RootMenu.GetCheckbox("enabled"))
+                return;
+            if (ObjectManager.Me.IsDead)
+                return;
+
+            Orbwalker.MoveTo(ObjectManager.Me.Team == GameObjectTeam.Order ? ChaosSpawn : OrderSpawn);
+        }
+    }
+}
---
+++ Run it down mid/Run it down mid/Utils.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using HesaEngine.SDK;
+
+namespace Run_it_down_mid
+{
+    public class Utils
+    {
+        public static void PrintChat(string msg)
+        {
+            Chat.Print("<font color = \"#ffdead\">[Feeder]:</font> <font color = \"#ffffff\">" + msg + "</font>");
+        }
+
+        public static string GetVersion()
+        {
+            string ver = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            return ver.Remove(ver.LastIndexOf('.'));
+        }
+
+
+    }
+
+    public static class MenuExtension
+    {
+        public static bool GetCheckbox(this Menu c, string name)
+        {
+            return c.Get<MenuCheckbox>(name).Checked;
+        }
+
+        public static int GetSlider(this Menu c, string name)
+        {
+            return c.Get<MenuSlider>(name).CurrentValue;
+        }
+
+        public static int GetCombobox(this Menu c, string name)
+        {
+            return c.Get<MenuCombo>(name).CurrentValue;
+        }
+    }
+}
